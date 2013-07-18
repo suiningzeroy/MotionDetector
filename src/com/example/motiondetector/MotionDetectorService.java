5382c6cd3eb3cf138cddda1b9ef4f828a2c4a4c5
zeroy <todezou18@163.com>
2013-07-18 22:12:31 +0800

    measure the accelerometer for a while,get the max acceleration and make judgement for movement.

--- src/com/example/motiondetector/MotionDetectorService.java
+++ src/com/example/motiondetector/MotionDetectorService.java
@@ -6,10 +6,13 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
 import android.os.IBinder;
 
 public class MotionDetectorService extends Service {
 	private static final String TAG = "MotionDetectorService";
+	private static final int MEASURE_DURATION = 1000*5;
+	private static final float ACCELERATION_OF_MOVE = 0.5f;
 	
 	private SensorManager sensorManager;
 	private final double calibration = SensorManager.STANDARD_GRAVITY;
@@ -34,10 +37,51 @@ public class MotionDetectorService extends Service {
 	};
 	
 	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		
+		
+		return Service.START_STICKY;
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	private void measureAcceleratometer(int delayMIllis){
+		Handler handler = new Handler();
+		Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		Runnable autoStop = new Runnable(){
+			@Override
+			public void run() {
+				sensorManager.unregisterListener(sensorEventListener);
+				writeDataInDB();
+			}
+		};
+		
+		sensorManager.registerListener(sensorEventListener,
+				accelerometer,
+				SensorManager.SENSOR_DELAY_FASTEST);
+		handler.postDelayed(autoStop, delayMIllis);
+	}
+	
+	private boolean isMoving(float acceleration){
+		return acceleration > ACCELERATION_OF_MOVE ? true : false;
+	}
+	
+	private void writeDataInDB(){
+		
+	}
 
 }

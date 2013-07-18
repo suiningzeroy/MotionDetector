e6a15e50987a81a7ccb24203358dddbb90b5b179
zeroy <todezou18@163.com>
2013-07-18 19:59:04 +0800

    modified the layout and resource in the activity

--- src/com/example/motiondetector/MovingTimeActivity.java
+++ src/com/example/motiondetector/MovingTimeActivity.java
@@ -3,20 +3,19 @@ package com.example.motiondetector;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MovingTimeActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_moving_time);
+		final TextView movingTime = (TextView) findViewById(R.id.movingTime);
+		final TextView percentage = (TextView) findViewById(R.id.percentage);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.moving_time, menu);
-		return true;
-	}
+
 
 }

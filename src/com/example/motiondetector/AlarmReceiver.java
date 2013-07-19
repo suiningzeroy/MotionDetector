d7c8bf3e2e1907e082c7285115755de7a595f1d3
zeroy <todezou18@163.com>
2013-07-19 21:10:32 +0800

    schedule Service by alarmmanager and wake device by Power lock

---
+++ src/com/example/motiondetector/AlarmReceiver.java
@@ -0,0 +1,49 @@
+package com.example.motiondetector;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+	private static PowerManager.WakeLock wakeLock = null;
+	private static final String LOCK_TAG = "com.example.motiondetector";
+	
+	/**
+	 * Method used to share the <code>WakeLock</code> created by this 
+	 * <code>BroadcastReceiver</code>. 
+	 * Note: this method is <code>synchronized</code> as it lazily creates
+	 * the <code>WakeLock</code>
+	 * 
+	 * @param 		ctx			The <code>Context</code> object acquiring the
+	 * 							lock.
+	 */
+	public static synchronized void acquireLock(Context ctx){
+		if (wakeLock == null){
+			PowerManager mgr = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+			wakeLock = mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, LOCK_TAG);
+			wakeLock.setReferenceCounted(true);
+		}
+		wakeLock.acquire();
+	}
+	
+	/**
+	 * Method used to release the shared <code>WakeLock</code>.
+	 */
+	public static synchronized void releaseLock(){
+		if (wakeLock != null){
+			wakeLock.release();
+		}
+	}
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		acquireLock(context);
+		Intent motionDetectorService = 
+			new Intent(context, MotionDetectorService.class);
+		context.startService(motionDetectorService);
+
+	}
+
+}
---
+++ src/com/example/motiondetector/MotionDetectorStartUpReceiver.java
@@ -0,0 +1,29 @@
+package com.example.motiondetector;
+
+import java.util.Calendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MotionDetectorStartUpReceiver extends BroadcastReceiver {
+
+	private static final int FIVE_MINUTES = 5*60*1000;
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		AlarmManager mgr = 
+				(AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+			Intent i = new Intent(context, AlarmReceiver.class);
+			PendingIntent sender = PendingIntent.getBroadcast(context, 0, 
+					i, PendingIntent.FLAG_CANCEL_CURRENT);
+			Calendar now = Calendar.getInstance();
+			now.add(Calendar.MINUTE, 2);
+			mgr.setRepeating(AlarmManager.RTC_WAKEUP, 
+					now.getTimeInMillis(), FIVE_MINUTES, sender);
+
+	}
+
+}
